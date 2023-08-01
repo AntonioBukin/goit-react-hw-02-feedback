99a3669599a2561614af106278afa5d065be7509
AntonioBukin <bav1905@gmail.com>
2023-08-01 12:31:38 +0300

    add styles

--- src/Vote/VoteActions/VoteActions.jsx
+++ src/Vote/VoteActions/VoteActions.jsx
@@ -1,11 +1,12 @@
-import styles from "../vote.module.scss";
+//import styles from "../vote.module.scss";
+import styles from "../VoteBlock/vote-block.module.scss"
 
 const VoteActions = ({leaveVote}) => {
     return (
        <>
-        <button onClick={() => leaveVote("good")} >Good</button>
-        <button onClick={() => leaveVote("neutral")}>Neutral</button>
-        <button onClick={() => leaveVote("bad")}>Bad</button>
+        <button className={styles.btn} onClick={() => leaveVote("good")} >Good</button>
+        <button className={styles.btn} onClick={() => leaveVote("neutral")}>Neutral</button>
+        <button className={styles.btn} onClick={() => leaveVote("bad")}>Bad</button>
        </>
             
         
--- src/Vote/VoteNotification/VoteNotification.jsx
+++ src/Vote/VoteNotification/VoteNotification.jsx
@@ -1,5 +1,3 @@
-import styles from "../vote.module.scss";
-
 const Notification = ({message}) => <p>{message}</p>;
 
 export default Notification;
